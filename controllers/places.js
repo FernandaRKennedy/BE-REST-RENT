a861fd995ad3d8dc711888a2c909c8185d5130a0
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-05-02 21:15:08 -0400

    correcting the README

--- controllers/places.js
+++ controllers/places.js
@@ -7,7 +7,8 @@ router.get('/', (req,res) => {
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
         pic: 'http://placekitten.com/250/250'
-      }, {
+      }, 
+      {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
