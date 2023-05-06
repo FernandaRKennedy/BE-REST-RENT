af1eb31b0cb1ba3be8a51dff3dcd747368006daf
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-05-05 21:56:42 -0400

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/brick-wall-1834784_960_720.jpeg'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: '/images/coffee-1711012_960_720.jpeg'
+      }]
