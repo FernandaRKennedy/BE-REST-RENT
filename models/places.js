76cbc752361594077e134da2b0753f5f813bd575
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-05-10 21:26:19 -0400

    adding mongoose

--- models/places.js
+++ models/places.js
@@ -1,13 +1,13 @@
-module.exports = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/brick-wall-1834784_960_720.jpeg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-1711012_960_720.jpeg'
-      }]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: {type: String},
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded:{type: Number} 
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -26,10 +26,11 @@ function new_form() {
                             <label htmlFor="state">State</label>
                             <input className="form-control" id="state" name="state" />
                         </div>
-                        <div className="form-group col-sm-4">
-                            <label htmlFor="founded">Founded Year</label>
-                            <input type="number" className="form-control" id="founded" name="founded" defaultValue={new Date().getFullYear()} />
+                        <div className="form-group">
+                            <label for="founded">Founded Year</label>
+                            <input className="form-control" id="founded" name="founded" />
                         </div>
+
                     </div>
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
